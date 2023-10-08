507d1b6139d064ce1bde808effb7b1f369c193db
unknown <michel.igiraneza@auca.ac.rw>
2023-10-08 18:31:50 +0200

    First commit

---
+++ src/main/java/com/inyangedemo/InyangeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.inyangedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyangeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InyangeDemoApplication.class, args);
+	}
+
+}
